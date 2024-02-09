58875459f0d971ab41a8ab0c2f8470a55f8d5b70
Mahpudin <pudinmah69@gmail.com>
2024-02-10 03:23:32 +0700

    Section 6: Membuat Kata Sambutan Component

--- src/app/template/portofolio-web/portofolio-web.component.ts
+++ src/app/template/portofolio-web/portofolio-web.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '@components/portofolio-web/header/header.component';
 import { BannerComponent } from '@components/portofolio-web/banner/banner.component';
 import { HeaderMenuComponent } from '@components/portofolio-web/header-menu/header-menu.component';
+import { AboutUsComponent } from '@components/portofolio-web/about-us/about-us.component';
 
 @Component({
   selector: 'app-portofolio-web',
   standalone: true,
   imports: [
-    HeaderComponent,BannerComponent,HeaderMenuComponent],
+    HeaderComponent,BannerComponent,HeaderMenuComponent,AboutUsComponent],
   templateUrl: './portofolio-web.component.html',
   styleUrl: './portofolio-web.component.css'
 })
